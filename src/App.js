98f00842facd58f78536634c9cb62b970709612f
Artiom Manokhin <amanokhin22@ukr.net>
2023-09-22 17:50:19 +0300

    It`s a front-end part for blog project. React, JS, hooks, Redux/toolkit and many others libraries

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import Container from "@mui/material/Container";
+
+import {Header} from "./components";
+import {Home, FullPost, Registration, AddPost, Login} from "./pages";
+import {Route, Routes} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect} from "react";
+import {fetchAuthMe, selectIsAuth} from "./redux/slices/auth";
+
+function App() {
+    const dispatch = useDispatch();
+    const isAuth = useSelector(selectIsAuth);
+
+    useEffect(() => {
+        dispatch(fetchAuthMe(isAuth));
+    }, []);
+
+    return (
+        <>
+            <Header/>
+            <Container maxWidth="lg">
+               <Routes>
+                   <Route path="/" element={<Home/>} ></Route>
+                   <Route path="/posts/:id" element={<FullPost/>} ></Route>
+                   <Route path="/posts/:id/edit" element={<AddPost/>} ></Route>
+                   <Route path="/add-post" element={<AddPost/>} ></Route>
+                   <Route path="/login" element={<Login/>} ></Route>
+                   <Route path="/register" element={<Registration/>} ></Route>
+               </Routes>
+            </Container>
+        </>
+    );
+}
+
+export default App;
